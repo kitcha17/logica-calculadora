e63d241b0b07ad4e15d0cefaf2fc950a0766d601
kitcha17 <cristian_andrzejewski@hotmail.com>
2022-06-23 00:48:37 -0300

    Envio

---
+++ calculadora.js
@@ -0,0 +1,55 @@
+function calculadora() {
+    const operacao = Number(prompt('Escolha uma operação:\n 1 - Soma \n 2 - Subtracao \n 3 - Multiplicação \n 4 - Divisão Real \n 5 - Divisão Inteira \n 6 - Potenciação'));
+
+    let n1 = Number(prompt('Insira o primeiro valor: '));
+    let n2 = Number(prompt('Insira o segundo valor: '));
+    let resultado;
+
+    function soma() {
+        resultado = n1 + n2
+        alert(`${n1}+${n2} = ${resultado}`)
+    }
+
+    function subtracao() {
+        resultado = n1 - n2
+        alert(`${n1}-${n2} = ${resultado}`)
+    }
+
+    function multiplicacao() {
+        resultado = n1 * n2
+        alert(`${n1}*${n2} = ${resultado}`)
+    }
+
+    function divisao_real() {
+        resultado = n1 / n2
+        alert(`${n1}/${n2} = ${resultado}`)
+    }
+
+    function divisao_inteira() {
+        resultado = n1 % n2
+        alert(`O resto da divisão entre ${n1} e ${n2} é igual a ${resultado}`)
+    }
+
+    function potenciacao() {
+        resultado = n1 ** n2
+        alert(`${n1} elevado a ${n2} é igual a ${resultado}`)
+    }
+
+
+    if (operacao == 1) {
+        soma();
+    } else if (operacao == 2) {
+        subtracao();
+    } else if (operacao == 3) {
+        multiplicacao();
+    } else if (operacao == 4) {
+        divisao_real();
+    } else if (operacao == 5) {
+        divisao_inteira();
+    } else if (operacao == 6) {
+        potenciacao();
+    }
+
+}
+
+calculadora()
